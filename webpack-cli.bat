15a543e940d8afc6ddad4677c7e046e7db3ca05a
lkeff <killinski.s@icloud.com>
2024-12-16 00:12:18 +0100

    1nn

---
+++ webpack-cli.bat
@@ -0,0 +1,27 @@
+[webpack-cli] Failed to load 'C:\Users\Administrator\OneDrive\Documents\GitHub\airhornbot\website\webpack.config.js' config
+[webpack-cli] Error: Cannot find module 'mini-css-extract-plugin'
+Require stack:
+- C:\Users\Administrator\OneDrive\Documents\GitHub\airhornbot\website\webpack.config.js
+- C:\Users\Administrator\OneDrive\Documents\GitHub\airhornbot\website\node_modules\webpack-cli\lib\webpack-cli.js     
+- C:\Users\Administrator\OneDrive\Documents\GitHub\airhornbot\website\node_modules\webpack-cli\lib\bootstrap.js       
+- C:\Users\Administrator\OneDrive\Documents\GitHub\airhornbot\website\node_modules\webpack-cli\bin\cli.js
+- C:\Users\Administrator\OneDrive\Documents\GitHub\airhornbot\website\node_modules\webpack\bin\webpack.js
+    at Module._resolveFilename (node:internal/modules/cjs/loader:1248:15)
+    at Module._load (node:internal/modules/cjs/loader:1074:27)
+    at TracingChannel.traceSync (node:diagnostics_channel:315:14)
+    at wrapModuleLoad (node:internal/modules/cjs/loader:217:24)
+    at Module.require (node:internal/modules/cjs/loader:1339:12)
+    at require (node:internal/modules/helpers:135:16)
+    at Object.<anonymous> (C:\Users\Administrator\OneDrive\Documents\GitHub\airhornbot\website\webpack.config.js:5:30)
+    at Module._compile (node:internal/modules/cjs/loader:1546:14)
+    at Module._extensions..js (node:internal/modules/cjs/loader:1691:10)
+    at Module.load (node:internal/modules/cjs/loader:1317:32) {
+  code: 'MODULE_NOT_FOUND',
+  requireStack: [
+    'C:\\Users\\Administrator\\OneDrive\\Documents\\GitHub\\airhornbot\\website\\webpack.config.js',
+    'C:\\Users\\Administrator\\OneDrive\\Documents\\GitHub\\airhornbot\\website\\node_modules\\webpack-cli\\lib\\webpack-cli.js',
+    'C:\\Users\\Administrator\\OneDrive\\Documents\\GitHub\\airhornbot\\website\\node_modules\\webpack-cli\\lib\\bootstrap.js',
+    'C:\\Users\\Administrator\\OneDrive\\Documents\\GitHub\\airhornbot\\website\\node_modules\\webpack-cli\\bin\\cli.js',
+    'C:\\Users\\Administrator\\OneDrive\\Documents\\GitHub\\airhornbot\\website\\node_modules\\webpack\\bin\\webpack.js'
+  ]
+}
